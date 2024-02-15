2eeea5f91e00d025271cae06f1764aa268ec2c7e
bendanwwww <461806307@qq.com>
2024-02-15 16:50:55 +0800

    web add controller

--- web/data_controller.py
+++ web/data_controller.py
@@ -1,10 +1,10 @@
 import json
 import sys
 
-from frame.constant.backtest_constant import Backtest
-
 sys.path.append('../basic-a-data-frame')
 
+from flask_cors import CORS
+from frame.constant.backtest_constant import Backtest
 from flask import Flask, request
 
 from frame.constant.code_collection_constant import CodeCollection
@@ -12,6 +12,7 @@ from frame.constant.strategy_constant import Strategy
 from frame.data.data_choose.data_choose import DataChoose
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 data_choose_service = DataChoose()
 
@@ -19,8 +20,14 @@ data_choose_service = DataChoose()
 @app.route('/commonTool/getStrategyList')
 def get_strategy_list():
     res_list = []
-    for v in Strategy.strategy_dict.values():
-        res_list.append(v.strategy_name)
+    index = 0
+    for k in Strategy.strategy_dict:
+        res_list.append({
+            'index': index,
+            'strategy_code': k,
+            'strategy_name': Strategy.strategy_dict[k].strategy_name,
+        })
+        index += 1
     return json.dumps(res_list)
 
 # 量化策略回测
@@ -32,7 +39,7 @@ def strategy_backtest():
     code_collection = request.args.get("codeCollection")
     if strategy not in Strategy.strategy_dict or code_collection not in CodeCollection.code_collection_dict:
         return json.dumps([])
-    res_table = Backtest.backtest_dict[backtest](Strategy.strategy_dict[strategy].strategy_func, CodeCollection.code_collection_dict[code_collection], backtest_day)
+    res_table = Backtest.backtest_dict[backtest].backtest_func(Strategy.strategy_dict[strategy].strategy_func, CodeCollection.code_collection_dict[code_collection], backtest_day)
     return json.dumps(res_table.to_dict(orient='list'))
 
 # 选择股票
